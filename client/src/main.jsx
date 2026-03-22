312ce29ba1e2ef3f4e00f4c0422a0e86f8cf8074
peraam-1708 <peraambigaidhevi@gmail.com>
2026-03-22 21:54:28 +0530

    add vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
+
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
